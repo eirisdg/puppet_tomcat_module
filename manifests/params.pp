a4dd0281cfd37f317b91ec4e924bf209e8e68e43
Mario Adrián Domínguez González <tupacsoul@gmail.com>
2017-12-06 15:13:20 +0100

    Parametrizado para distintos SO

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,23 @@
 # == Class: tomcat::params
 #
 class tomcat::params {
-    $packages       = 'tomcat8'
-    $user           = 'root'
-    $group          = 'root'
-    $config_path    = '/etc/default/tomcat'
-    $service_name   = 'tomcat8'
-    $service_state  = running
+    case $::os['family'] {
+		'Debian' : {
+			$packages = 'tomcat8'
+            $user     = 'root'
+            $group    = 'root'
+            $config_path    = '/etc/default/tomcat'
+            $service_name   = 'tomcat8'
+		}
+		'RedHat' : {
+			$packages = 'tomcat'
+            $user     = 'tomcat'
+            $group    = 'tomcat'
+            $config_path    = '/etc/default/tomcat'
+            $service_name   = 'tomcat'
+		}
+	}
 
+    $service_state  = running
 
 }
